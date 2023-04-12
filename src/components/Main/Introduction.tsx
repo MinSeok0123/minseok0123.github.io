306d9b00009efb27f998cb3f99082d9b30aec9f4
민석 <80371412+MinSeok0123@users.noreply.github.com>
2023-04-12 19:08:41 +0900

    Fix :: Darkmode

--- src/components/Main/Introduction.tsx
+++ src/components/Main/Introduction.tsx
@@ -4,6 +4,7 @@ import { IGatsbyImageData } from 'gatsby-plugin-image'
 import { toast, ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { useTheme } from '@skagami/gatsby-plugin-dark-mode'
+import { DarkModeSwitch } from 'react-toggle-dark-mode'
 
 type IntroductionProps = {
   profileImage: IGatsbyImageData
@@ -119,9 +120,16 @@ const Introduction: FunctionComponent<IntroductionProps> = function ({}) {
   const notify = () => toast('준비중입니다.')
   const [theme, toggleTheme] = useTheme()
 
+  const [isDarkMode, setDarkMode] = React.useState(true)
+
+  const toggleDarkMode = (checked: boolean) => {
+    setDarkMode(checked)
+  }
+
   if (theme === null) {
     return null
   }
+
   return (
     <HeaderCon>
       <ToastContainer
@@ -140,19 +148,19 @@ const Introduction: FunctionComponent<IntroductionProps> = function ({}) {
         <Logo href="/">minlog</Logo>
         <Nav>
           <Dark
-            onClick={() => toggleTheme(theme === 'light' ? 'dark' : 'light')}
+            onClick={() => {
+              toggleTheme(theme === 'light' ? 'dark' : 'light')
+              toggleDarkMode(!isDarkMode) // checked 인자 전달
+            }}
             role="checkbox"
             aria-checked={theme === 'dark'}
             tabIndex={0}
           >
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M20.6144 14.6145C19.787 14.8653 18.9093 15.0001 18 15.0001C13.0294 15.0001 9 10.9707 9 6.00013C9 5.09088 9.13484 4.21311 9.3856 3.38574C5.69007 4.50583 3 7.93883 3 12.0001C3 16.9707 7.02944 21.0001 12 21.0001C16.0613 21.0001 19.4943 18.3101 20.6144 14.6145Z"
-                fill="currentColor"
-              ></path>
-            </svg>
+            <DarkModeSwitch
+              checked={isDarkMode}
+              onChange={toggleDarkMode}
+              size={25}
+            />
           </Dark>
           <Search onClick={notify}>
             <svg width="17" height="17" viewBox="0 0 17 17">
