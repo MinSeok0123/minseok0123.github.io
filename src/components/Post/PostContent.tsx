b2dcb2ef05f111ebd12e287a874e538a17df1b6d
민석 <80371412+MinSeok0123@users.noreply.github.com>
2023-04-21 12:25:46 +0900

    fix :: markdown layout

--- src/components/Post/PostContent.tsx
+++ src/components/Post/PostContent.tsx
@@ -11,7 +11,7 @@ const MarkdownRenderer = styled.div`
   flex-direction: column;
   width: 768px;
   margin: 0 auto;
-  padding: 100px 0;
+  padding: 10px 0;
   word-break: break-all;
 
   // Markdown Style
