ac3b2ff7b31843be3df4490be5890a0a2610521e
민석 <80371412+MinSeok0123@users.noreply.github.com>
2023-04-26 19:49:00 +0900

    Fix :: about->notion

--- src/components/Main/Introduction.tsx
+++ src/components/Main/Introduction.tsx
@@ -166,19 +166,9 @@ const Introduction: FunctionComponent<IntroductionProps> = function ({}) {
               </svg>
             </Search>
           </a>
-          <About onClick={notify}>ABOUT</About>
-          <ToastContainer
-            position="top-right" // 알람 위치 지정
-            autoClose={2000} // 자동 off 시간
-            hideProgressBar={false} // 진행시간바 숨김
-            closeOnClick // 클릭으로 알람 닫기
-            rtl={false} // 알림 좌우 반전
-            pauseOnFocusLoss // 화면을 벗어나면 알람 정지
-            draggable // 드래그 가능
-            pauseOnHover // 마우스를 올리면 알람 정지
-            theme="light"
-            // limit={1} // 알람 개수 제한
-          />
+          <a href="https://successful-star-405.notion.site/Lee-MinSeok-1812001504044d3f9b8a31b731823db5">
+            <About>ABOUT</About>
+          </a>
         </Nav>
       </Header>
     </HeaderCon>
